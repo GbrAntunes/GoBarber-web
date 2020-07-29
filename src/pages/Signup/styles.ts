069d9ee642874478cee4afb17f8cefb1cbe71640
Gabriel antunes <gbr.antunes@gmail.com>
2020-07-29 17:01:30 -0400

    Rotas entre cadastro e login concluídas; Animações adicionadas

--- src/pages/Signup/styles.ts
+++ src/pages/Signup/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import { shade } from 'polished'
 
 import signUpBackgroundImg from '../../assets/sign-up-background.png'
@@ -14,12 +14,30 @@ export const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-
-  /* align-items e justify-content */
-  place-content: center;
+  justify-content: center;
 
   width: 100%;
   max-width: 700px;
+`
+
+const appearFromRight = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${appearFromRight} 1s;
 
   form {
     margin: 80px 0;
@@ -45,7 +63,7 @@ export const Content = styled.div`
 
   /* Seleciona apenas os elementos a que vierem diretamente de Content (os filhos a, de Content) */
   > a {
-    color: #F4EDE8;
+    color: #FF9000;
     display: block;
     margin-top: 24px;
     text-decoration: none;
@@ -59,7 +77,7 @@ export const Content = styled.div`
     }
 
     &:hover {
-      color: ${shade(0.2, '#F4EDE8')};
+      color: ${shade(0.2, '#FF9000')};
     }
   }
 `
